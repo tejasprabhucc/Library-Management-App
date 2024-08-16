6842b6e5a802b147b85e56f8676c655aa6f99e53
tejasprabhucc <163384136+tejasprabhucc@users.noreply.github.com>
2024-08-16 19:55:24 +0530

    Created react app and configurations (#3)
    Created a react app

---
+++ Frontend/plopfile.js
@@ -0,0 +1,24 @@
+export default function (plop) {
+  plop.setGenerator("component", {
+    description: "Generate a React component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name:",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/Component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.ts",
+        templateFile: "plop-templates/Component.stories.ts.hbs",
+      },
+    ],
+  });
+}
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="w-full"></div>
+    </>
+  );
+}
+
+export default App;
